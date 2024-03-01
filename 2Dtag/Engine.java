a72f4667eadb909c82f83966c2fd83c6dba5a857
Faith Dennis <32501997+faithdennis@users.noreply.github.com>
2024-03-01 13:18:34 -0800

    Update Engine.java

--- 2Dtag/Engine.java
+++ 2Dtag/Engine.java
@@ -60,8 +60,6 @@ public class Engine {
 
             // plays game
             playGame(bg, data);
-
-            // add graphic for game over, winning player
         }
 
         if (curr.equals('L') || curr.equals('l')) {
@@ -179,36 +177,10 @@ public class Engine {
         ter.renderFrame(bg.berkeleyWorld.getCampus());
         playGame(bg, data);
     }
-
-    /**
-     * Method used for autograding and testing your code. The input string will be a series
-     * of characters (for example, "n123sswwdasdassadwas", "n123sss:q", "lwww". The engine should
-     * behave exactly as if the user typed these characters into the engine using
-     * interactWithKeyboard.
-     *
-     * Recall that strings ending in ":q" should cause the game to quite save. For example,
-     * if we do interactWithInputString("n123sss:q"), we expect the game to run the first
-     * 7 commands (n123sss) and then quit and save. If we then do
-     * interactWithInputString("l"), we should be back in the exact same state.
-     *
-     * In other words, running both of these:
-     *   - interactWithInputString("n123sss:q")
-     *   - interactWithInputString("lww")
-     *
-     * should yield the exact same world state as:
-     *   - interactWithInputString("n123sssww")
-     *
-     * @param input the input string to feed to your program
-     * @return the 2D TETile[][] representing the state of the world
-     */
+    
     public TETile[][] interactWithInputString(String input) {
-        // TODO: Fill out this method so that it run the engine using the input
-        // passed in as an argument, and return a 2D tile representation of the
-        // world that would have been drawn if the same inputs had been given
-        // to interactWithKeyboard().
-        //
-        // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
-        // that works for many different input types.
+        /* interactWithInputString runs the engine using the input, and returns a 2D tile representation of the
+    world that would have been drawn if the same inputs had been given to interactWithKeyboard().*/
         loadGame(input);
 
         TETile[][] finalWorldFrame = null;
